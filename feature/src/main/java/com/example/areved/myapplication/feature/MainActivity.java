80b2738760c8eb437d51995eb9e88d653e8b9061
0v3rt1m3 <jardevera@gmail.com>
2018-08-27 20:12:49 +0800

    first commit

---
+++ feature/src/main/java/com/example/areved/myapplication/feature/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.areved.myapplication.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
